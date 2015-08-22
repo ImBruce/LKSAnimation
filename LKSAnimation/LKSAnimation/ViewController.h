63411ff2010f38cf414463c37551e8c12ffc6c31
guobingwei <guobingwei@10.254.2.61>
2015-08-22 15:07:04 +0800

    <提交代码>

---
+++ LKSAnimation/LKSAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKSAnimation
+//
+//  Created by guobingwei on 15/8/22.
+//  Copyright (c) 2015年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LKSAnimation/LKSAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LKSAnimation
+//
+//  Created by guobingwei on 15/8/22.
+//  Copyright (c) 2015年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
